7ea37b67c86c418a9a2fd793a1c77834385cd858
Nico Hahn <Nico Hahn>
2020-03-27 09:36:03 +0100

    story telling updated

--- server/create_story.R
+++ server/create_story.R
@@ -17,25 +17,32 @@ infected_world <- daily_cases[daily_cases$date == max(daily_cases$date), ]$confi
 died <- daily_cases[daily_cases$date == max(daily_cases$date), ]$deaths
 file_1[14] <- paste("To date", infected_world, "people have been infected with the virus and", died, "have died.")
 today <- daily_cases2[daily_cases2$date == max(daily_cases$date), ]
+country <- as.character(today[today$confirmed == max(today$confirmed), 2])
+if (country == "US") {
+  country <- "the United States"
+}
 file_2[8] <- paste(
   "In ",
-  as.character(today[today$confirmed == max(today$confirmed), 2]),
+  country,
   ", ",
   as.character(today[today$confirmed == max(today$confirmed), 3]),
   " people are infected with the virus, making it the country with the most infections worldwide.",
   sep = ""
 )
-
+country <- as.character(today[today$deaths == max(today$deaths), 2])
+if (country == "US") {
+  country <- "the United States"
+}
 file_3[8] <- paste(
   as.character(today[today$deaths == max(today$deaths), 4]),
   "people have died in",
-  as.character(today[today$deaths == max(today$deaths), 2]),
+  country,
   "as a result of the coronavirus. <br>"
 )
 
 file_3[9] <- paste(
   "This makes",
-  as.character(today[today$deaths == max(today$deaths), 2]),
+  country,
   "the country with the most deaths attributable to the virus."
 )
 
