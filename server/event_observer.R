dac94c39edde798e89fe5f64aefc77a0bc22fad4
Nico Hahn <nico.hahn@de.pwc.com>
2020-06-29 08:28:19 +0200

    removed prints

--- server/event_observer.R
+++ server/event_observer.R
@@ -9,8 +9,6 @@ observeEvent(list(
     daterange <- get_date()
     # try and get the country
     country <- try(get_country(), silent = TRUE)
-    print(daterange)
-    print(country)
     corona_frame <- st_as_sf(corona_sf, crs = 4326)
     if (class(country) != "try-error") {
       # if a country was clicked, select the subset of corona data
